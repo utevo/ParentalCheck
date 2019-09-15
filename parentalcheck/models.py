4e578c1c615d534cf85ff1631b60dc939e9a7cab
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-15 17:40:22 +0200

    Update models.

--- parentalcheck/models.py
+++ parentalcheck/models.py
@@ -1,61 +1,82 @@
+import uuid
+
 from django.db import models
-import os
+
 
 class DangerType(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
-NONE = 'N'
-LOW = 'L'
-MEDIUM = 'M'
-HIGHT = 'H'
-DANGER_LEVEL = (
-    (NONE, 'None'),
-    (LOW, 'Low'),
-    (MEDIUM, 'Medium'),
-    (HIGHT, 'Hight'),
-)
 
 class Danger(models.Model):
     danger_type = models.ForeignKey(DangerType, on_delete=models.CASCADE)
 
+    NONE = 'N'
+    LOW = 'L'
+    MEDIUM = 'M'
+    HIGHT = 'H'
+
+    DANGER_LEVEL_CHOICES = (
+    (NONE, 'None'),
+    (LOW, 'Low'),
+    (MEDIUM, 'Medium'),
+    (HIGHT, 'Hight'),
+    )
+
     danger_level = models.CharField(
         max_length=1,
-        choices=DANGER_LEVEL,
+        choices=DANGER_LEVEL_CHOICES,
         default=NONE
     )
 
+    def __str__(self):
+        return '{0}: {1}'.format(self.danger_type, self.danger_level)
 
-SITE = 'S'
-INSTAGRAM_ACCOUNT = 'I_A'
-YOUTUBE_CHANNEl = 'Y_C'
-FACEBOOK_GROUP = 'F_G'
-FACEBOOK_ACCOUNT = 'F_A'
-TWITTER_ACCOUNT = 'T_A'
-REDDIT_THREAD = 'R_T'
-
-CONTENT_TYPE = (
-    (SITE, 'Site'),
-    (INSTAGRAM_ACCOUNT, 'Instagram Account'),
-    (YOUTUBE_CHANNEl, 'Youtube Channel'),
-    (FACEBOOK_GROUP, 'Facebook Group'),
-    (FACEBOOK_ACCOUNT, 'Facebook Account'),
-    (TWITTER_ACCOUNT, 'Twitter Account'),
-    (REDDIT_THREAD, 'Reddit Thread'),
-)
 
 def get_image_path(instance, filename): # instance is None before create!
-    return os.path.join('photos', str(instance.id), filename)
+    from os import path
+    file_ext = path.splitext(filename)[1]
+    return path.join('photos', str(instance.id), 
+                        'avatar' + file_ext )
 
 class Content(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
     dangers = models.ManyToManyField(Danger, blank=True)
 
-    content_type = models.CharField(
-        max_length=3,
-        choices=CONTENT_TYPE,
+
+    SITE = 'S'
+    INSTAGRAM_ACCOUNT = 'I_A'
+    YOUTUBE_CHANNEl = 'Y_C'
+    FACEBOOK_GROUP = 'F_G'
+    FACEBOOK_ACCOUNT = 'F_A'
+    TWITTER_ACCOUNT = 'T_A'
+    REDDIT_THREAD = 'R_T'
+
+    CONTENT_TYPE_CHOICES = (
+        (SITE, 'Site'),
+        (INSTAGRAM_ACCOUNT, 'Instagram Account'),
+        (YOUTUBE_CHANNEl, 'Youtube Channel'),
+        (FACEBOOK_GROUP, 'Facebook Group'),
+        (FACEBOOK_ACCOUNT, 'Facebook Account'),
+        (TWITTER_ACCOUNT, 'Twitter Account'),
+        (REDDIT_THREAD, 'Reddit Thread'),
     )
 
-    image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+    content_type = models.CharField(
+        max_length=3,
+        choices=CONTENT_TYPE_CHOICES,
+    )
+
+    image = models.ImageField(
+        default='photos/default',
+        upload_to=get_image_path, 
+        blank=True, null=True
+        )
+
+    def __str__(self):
+        return '{0} [{1}]'.format(self.name, self.content_type)
